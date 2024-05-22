d213221d2d4f02975e56504ab93c19449d5b4ead
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-05-22 12:07:31 -0700

    umu_debug: fix bug when building flatpak command

--- umu/umu_debug.py
+++ umu/umu_debug.py
@@ -16,21 +16,10 @@ def flatpak_run_in_host(
 
     Will execute the system umu-launcher instead the one in the Flatpak
     """
-    log.warning("Will execute flatpak-spawn")
-    log.warning("Changing prefix: %s -> %s", root.parent, "/usr")
-    log.warning("Assuming system umu-launcher is installed")
-
-    root: Path = Path("/usr/share/umu")
     verb: str = env["PROTON_VERB"]
     flatpak_bin: str = which("flatpak-spawn")
 
-    if not FLATPAK_PATH or env.get("UMU_CONTAINER") != "0" or not flatpak_bin:
-        log.warning("Will not execute flatpakk-spawn")
-        log.warning("FLATPAK_PATH: %s", FLATPAK_PATH)
-        log.warning("UMU_CONTAINER: %s", env.get("UMU_CONTAINER"))
-        log.warning("flatpak-spawn: %s", flatpak_bin)
-        log.warning("Reverting prefix: %s -> %s", root.parent, "/app")
-        root = Path("/app/share/umu")
+    if env.get("UMU_CONTAINER") != "0":
         if opts:
             command.extend(
                 root.joinpath("reaper").as_posix(),
@@ -62,13 +51,18 @@ def flatpak_run_in_host(
         )
         return command
 
+    log.warning("Will execute flatpak-spawn")
+    log.warning("Changing prefix: %s -> %s", root.parent, "/usr")
+    log.warning("Assuming system umu-launcher is installed")
+    root = Path("/usr/share/umu")
+
     if opts:
         command.extend(
             flatpak_bin,
             *[
                 f"--env={var}={os.environ.get(var)}"
                 for var in os.environ
-                if var in env or var.startswith("GAMESCOPE", "DISPLAY")
+                if var in env or var.startswith(("GAMESCOPE", "DISPLAY"))
             ],
             "--host",
             root.joinpath("reaper").as_posix(),
@@ -90,7 +84,7 @@ def flatpak_run_in_host(
             *[
                 f"--env={var}={os.environ.get(var)}"
                 for var in os.environ
-                if var in env or var.startswith("GAMESCOPE", "DISPLAY")
+                if var in env or var.startswith(("GAMESCOPE", "DISPLAY"))
             ],
             "--host",
             root.joinpath("reaper").as_posix(),
